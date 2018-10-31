22ae33a96342cf68102fad4886cbccd5234d0ea4
Keshav Bathla <keshavbathla2017@gmail.com>
2018-10-31 04:05:14 -0400

    continue

---
+++ javascript/product.js
@@ -0,0 +1,17 @@
+var backgound_image_index = 1;
+
+function go_home()
+  {
+    location.replace("http://localhost/gym/php/home.php");  
+  }
+  
+function change_background_image_towards_left()
+  {
+    if(backgound_image_index>=1 && backgound_image_index<3)
+      {
+        backgound_image_index += 1
+        document.getElementById("bottom").background-image = "url(http://localhost/gym/images/image" + backgound_image_index + ".jpeg);" ;
+      } 
+  }
+  
+
