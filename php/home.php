a42bebcc6c4281c5b4389defa574ed045377cf82
Keshav Bathla <keshavbathla2017@gmail.com>
2018-11-10 07:54:17 -0500

    added

--- php/home.php
+++ php/home.php
@@ -9,14 +9,16 @@
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<script src="http://localhost/gym/javascript/product.js"></script>
+		<script src="http://localhost/gym/javascript/home.js"></script>
 		<script>
-
+			function select_gender(){
+				document.getElementById("select_gender").show();
+			}
 		</script>
 	</head>
 	<body>
 		<div class="container-fluid top-bar">
-			<form method="post" action="http://localhost/gym/php/validation.php">
+			<form method="post" action="http://localhost/gym/php/product.php">
 				<div class="row">
 					<div class="col-md-5">
 						<img src="http://localhost/gym/images/logo-magic.png" class="logo">
@@ -38,42 +40,62 @@
 		</div>
 		<div class="container-fluid bottom">
 			<div class="container-fluid signup_template">
-				<div class="row">
-					<div class="col-md-4">
+				<form method="post" action="http://localhost/gym/php/validation.php">
+					<div class="row">
+						<div class="col-md-4" style="padding:0px; margin:0px;">
+							<img src="http://localhost/gym/images/fruits.jpg" class="img-fluid" style="max-width: 100%; height: 500%;">
+						</div>
+						<div class="col-md-8" >
+							<div class="row">
+								<div class="Col-md-12">
+									<p class="create_account_text">Create an account</p>
+								</div>
+							</div>
+							<div class="row" style="height: 40px; position: relative; margin-top: 30px; left: -10px;">
+								<div class="col-md-12">
+									<input type="text" class="name_input_box" placeholder="Name" name="name" id='1' oninput="name_method()" required>
+									<p class="error" id="11"></p>
+								</div>
+							</div>
+							<div class="row" style="height: 40px; position: relative; left: -10px;">
+								<div class="col-md-12">
+									<input type="text" class="name_input_box" placeholder="email" name="email" id='3' required oninput="email_method()">
+									<p class="error" id="33"></p>
+								</div>
+							</div>
+							<div class="row" style="height: 40px; position: relative; left: -10px;">
+								<div class="col-md-12">
+									<input type="text" class="name_input_box" placeholder="Mobile Number" required>
+								</div>
+							</div>
+							<div class="row" style="height: 40px; position: relative; left: -10px;">
+								<div class="col-md-12" id="select_gender">
+									<span class="gender_text">Gender</span>
+									<select class="gender_select_box">
+										<option value="male">Male</option>
+										<option value="female">Female</option>
+										<option value="other">Other</option>
+									</select>
+									<div class="select_arrow" onclick="select_gender()"></div>
+								</div>
+							</div>
+							<div class="row" style="height: 40px; position: relative; left: -10px;">
+								<div class="col-md-12">
+									<input type="reset" value="Reset" class="reset_button"><input type="submit" value="Next" class="next_button" id="submit_button"> 
+								</div>
+							</div>
+							<div class="row" style="height: 40px; position: relative; left: -10px;">
+								<div class="col-md-12">
+									<input type="button" class="small_dot_button" id="dot_1"><input type="button" class="small_dot_button" id="dot_2"><input type="button" class="small_dot_button" id="dot_3"> 
+								</div>
+							</div>
+							<script>
+								document.getElementById("dot_2").style.background = "#0074D9";
+								document.getElementById("dot_3").style.background = "#0074D9";
+							</script>
+						</div>
 					</div>
-					<div class="col-md-8">
-						<div class="row">
-							<div class="Col-md-12">
-								<p class="create_account_text">Create an account</p>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<input type="text" class="name_input_box" placeholder="Name">
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<input type="text" class="name_input_box" placeholder="email">
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<input type="text" class="name_input_box" placeholder="username">
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<input type="text" class="name_input_box" placeholder="Mobile Number">
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<input type="password" class="name_input_box" placeholder="Password">
-							</div>
-						</div>
-					</div>
-				</div>
+				</form>
 			</div>
 		</div>
 	</body>
